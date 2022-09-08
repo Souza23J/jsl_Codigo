3ae4221f2d65ea048ec066de3fc17d2db8ef9f92
Souza23J <113127272+Souza23J@users.noreply.github.com>
2022-09-08 15:10:03 -0300

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'no-use-before-define': 'off',
+  },
+  overrides: [
+    {
+      files: ['./*.mjs'],
+      rules: {
+        'import/no-extraneous-dependencies': [
+          'error',
+          { devDependencies: true },
+        ],
+      },
+    },
+  ],
+};
